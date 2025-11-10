90d925a197c75d6e7258a8a976ae206a21e20a80
Robert von Massow <robert.von.massow@superluminar.io>
2025-11-10 10:14:17 +0100

    cleanup for linter

--- lib/budget-alerts-stack.ts
+++ lib/budget-alerts-stack.ts
@@ -7,10 +7,8 @@ import {
   aws_budgets as budgets,
   custom_resources as cr,
   aws_iam as iam,
-  aws_kms as kms,
   aws_lambda as lambda,
   aws_s3 as s3,
-  aws_sns as sns,
 } from 'aws-cdk-lib';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import type { StackSetStackProps } from 'cdk-stacksets';
@@ -25,8 +23,6 @@ import {
 import type { Construct } from 'constructs';
 
 const ORG_ID = 'o-blikiivk10';
-const BUDGGET_ALERTS_TOPIC_NAME = 'BudgetAlerts';
-const BUDGET_ALERTS_ACCOUNT_ID = '391613010373';
 const DELEGATED_ADMIN_ACCOUNT_ID = '043443579270';
 
 export class BudgetAlertsStack extends Stack {
@@ -112,79 +108,10 @@ export class BudgetAlertsStack extends Stack {
       deploymentType: DeploymentType.serviceManaged(),
       capabilities: [Capability.NAMED_IAM],
     });
-    //const notifierStack = new BudgetNotifierStack(this, 'BudgetNotifierTemplate', {
-    //assetBuckets: [assetBucket],
-    //assetBucketPrefix: assetBucketPrefix,
-    //});
-    //const notifierStackSet = new StackSet(this, 'BudgetNotifierStackSet', {
-    //target: StackSetTarget.fromAccounts({
-    //accounts: [BUDGET_ALERTS_ACCOUNT_ID],
-    //regions: [this.region],
-    //}),
-    //template: StackSetTemplate.fromStackSetStack(notifierStack),
-    //deploymentType: DeploymentType.selfManaged({
-    //adminRole: iam.Role.fromRoleName(
-    //this,
-    //'AdminRole',
-    //'AWSCloudFormationStackSetAdministrationRole',
-    //),
-    //executionRoleName: 'AWSCloudFormationStackSetExecutionRole',
-    //}),
-    //capabilities: [Capability.NAMED_IAM],
-    //});
-    //alertStackSet.node.addDependency(notifierStackSet);
-    //notifierStackSet.node.addDependency(assetBucket);
     alertStackSet.node.addDependency(assetBucket);
     alertStackSet.node.addDependency(permissions);
   }
 }
-
-//class BudgetNotifierStack extends StackSetStack {
-//constructor(scope: Construct, id: string, props?: StackSetStackProps) {
-//super(scope, id, props);
-
-//const alertTopicKey = new kms.Key(this, 'AlertTopicKey', {
-//enableKeyRotation: true,
-//});
-//alertTopicKey.addToResourcePolicy(
-//new iam.PolicyStatement({
-//actions: [
-//'kms:Encrypt',
-//'kms:Decrypt',
-//'kms:ReEncrypt*',
-//'kms:GenerateDataKey*',
-//'kms:DescribeKey',
-//],
-//principals: [new iam.ServicePrincipal('budgets.amazonaws.com')],
-//resources: ['*'],
-//conditions: {
-//StringEquals: {
-//'aws:PrincipalOrgID': ORG_ID,
-//},
-//},
-//}),
-//);
-
-//const budgetAlertsTopic = new sns.Topic(this, 'BudgetAlertsTopic', {
-//displayName: 'Budget Alerts Topic',
-//topicName: BUDGGET_ALERTS_TOPIC_NAME,
-//masterKey: alertTopicKey,
-//});
-
-//budgetAlertsTopic.addToResourcePolicy(
-//new iam.PolicyStatement({
-//actions: ['SNS:Publish'],
-//principals: [new iam.ServicePrincipal('budgets.amazonaws.com')],
-//resources: ['*'],
-//conditions: {
-//StringEquals: {
-//'aws:PrincipalOrgID': ORG_ID,
-//},
-//},
-//}),
-//);
-//}
-//}
 
 class BudgetAlert extends StackSetStack {
   constructor(scope: Construct, id: string, props?: StackSetStackProps) {
@@ -232,18 +159,6 @@ class BudgetAlert extends StackSetStack {
             thresholdType: 'PERCENTAGE',
           },
           subscribers: [
-            //{
-            //subscriptionType: 'SNS',
-            //address: Arn.format({
-            //region: this.region,
-            //service: 'sns',
-            //resource: 'topic',
-            //resourceName: BUDGGET_ALERTS_TOPIC_NAME,
-            //account: BUDGET_ALERTS_ACCOUNT_ID,
-            //arnFormat: ArnFormat.SLASH_RESOURCE_NAME,
-            //partition: this.partition,
-            //}),
-            //},
             {
               subscriptionType: 'EMAIL',
               address: accountEmail,
